7bb925e0e4f32a571bb08355054c77aa5b33446d
mhassan000 <hassanasif946@gmail.com>
2023-05-19 04:41:57 +0500

    added plumbing services to plumbing page

---
+++ app/plumbing/page.js
@@ -0,0 +1,15 @@
+import PlumbingServices from '@/components/PlumbingServices'
+import React from 'react'
+
+const Plumbing = () => {
+  return (
+    <div>
+        <div>
+
+            <PlumbingServices />
+        </div>
+    </div>
+  )
+}
+
+export default Plumbing
