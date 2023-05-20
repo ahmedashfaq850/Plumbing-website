f9542ea330ba6a321a46862fb4dff90775050899
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-05-20 23:36:15 +0500

    added testimonial section

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = ({afterOurText}) => {
   return (
     <div className="drop-shadow-xl w-full text-center mb-10 sm:mb-20 text-2xl sm:text-4xl">
-      <h2 className="text-black font-extrabold">
+      <h2 className="text-black drop-shadow-2xl font-extrabold">
         OUR <span className="text-[#73D043]">{afterOurText}</span>
       </h2>
     </div>
