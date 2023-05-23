6aa8cf819d0932c3f09e532d59e13be410563d80
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-05-23 18:27:35 +0500

    updated contect form

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,56 +1,76 @@
-'use client';
+"use client";
 
-
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleSideNav = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleSideNav = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div id="home">
-        <header className="flex justify-between items-center w-full px-[20px] sm:px-[40px] md:px-[90px] h-[110px]">
-            <Link href="/">
-                <Image src="/images/logo.svg" alt="logo" width="200" height="200"/>
-            </Link>
+      <header className="flex justify-between items-center w-full px-[20px] sm:px-[40px] md:px-[90px] h-[110px]">
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="logo" width="200" height="200" />
+        </Link>
 
-            <div className='hidden lg:block'>
-                <nav className='flex gap-5 font-bold text-[20px] text-[#73D043]'>
-                    <Link href="#home">HOME</Link>
-                    <Link href="#services">Plumbing</Link>
-                    <Link href="#services">Electrics</Link>
-                    <Link href="#services">Heating</Link>
-                    <Link href="#services">Bathrooms</Link>
-                    <Link href="#services">Kitchens</Link>
-                    <Link href="#services">Refubs</Link>
-                    <Link href="#contact">CONTACT</Link>
-                </nav>
-            </div>
-            <Link onClick={toggleSideNav} className='lg:hidden cursor-pointer' href="/">
-            <Image src="/images/menuicon.svg" alt="" width={30} height={30}/>
-            </Link>
-        </header>
-    <div className={
-        isOpen ? "w-full h-screen bg-white  fixed top-0 left-0 z-10 duration-300 flex flex-col items-center pt-[200px] text-[25px] text-center font-bold "
-            : "w-full h-screen bg-white  fixed top-30 left-[-100%] z-10 duration-300"
-    }>
-            <Image onClick={toggleSideNav} className='absolute cursor-pointer hover:text-[#73D043] top-2 right-3' src="/images/CloseIcon.svg" alt="" width={30} height={30}/>
-            <nav className='flex flex-col gap-4 duration-200'>
-                <Link className='hover:text-[#73D043] duration-300' href="#home">HOME</Link>
-                <Link href="#services">Plumbing</Link>
-                    <Link href="#services">Electrics</Link>
-                    <Link href="#services">Heating</Link>
-                    <Link href="#services">Bathrooms</Link>
-                    <Link href="#services">Kitchens</Link>
-                    <Link href="#services">Refubs</Link>
-                <Link className='hover:text-[#73D043] duration-300' href="#contact">CONTACT</Link>
-            </nav>
+        <div className="hidden lg:block">
+          <nav className="flex gap-5 font-bold text-[20px] text-[#73D043]">
+            <Link href="#home">HOME</Link>
+            <Link href="#services">Plumbing</Link>
+            <Link href="#services">Electrics</Link>
+            <Link href="#services">Heating</Link>
+            <Link href="#services">Bathrooms</Link>
+            <Link href="#services">Kitchens</Link>
+            <Link href="#services">Refubs</Link>
+            <Link href="/contact/#contact">Contact</Link>
+          </nav>
         </div>
+        <Link
+          onClick={toggleSideNav}
+          className="lg:hidden cursor-pointer"
+          href="/"
+        >
+          <Image src="/images/menuicon.svg" alt="" width={30} height={30} />
+        </Link>
+      </header>
+      <div
+        className={
+          isOpen
+            ? "w-full h-screen bg-white  fixed top-0 left-0 z-10 duration-300 flex flex-col items-center pt-[200px] text-[25px] text-center font-bold "
+            : "w-full h-screen bg-white  fixed top-30 left-[-100%] z-10 duration-300"
+        }
+      >
+        <Image
+          onClick={toggleSideNav}
+          className="absolute cursor-pointer hover:text-[#73D043] top-2 right-3"
+          src="/images/CloseIcon.svg"
+          alt=""
+          width={30}
+          height={30}
+        />
+        <nav className="flex flex-col gap-4 duration-200">
+          <Link className="hover:text-[#73D043] duration-300" href="#home">
+            HOME
+          </Link>
+          <Link href="#services">Plumbing</Link>
+          <Link href="#services">Electrics</Link>
+          <Link href="#services">Heating</Link>
+          <Link href="#services">Bathrooms</Link>
+          <Link href="#services">Kitchens</Link>
+          <Link href="#services">Refubs</Link>
+          <Link
+            className="hover:text-[#73D043] duration-300"
+            href="/contact/#contact"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
     </div>
   );
 };
--- components/Heading.jsx
+++ components/Heading.jsx
@@ -10,7 +10,7 @@ const Heading = ({
     <div
       className={`drop-shadow-xl w-full text-center mb-10 sm:${mb} text-2xl sm:text-4xl`}
     >
-      <h2 className="text-black drop-shadow-2xl font-extrabold">
+      <h2 className="text-black tracking-widest drop-shadow-2xl font-extrabold">
         {our} <span className={`${textColor}`}>{afterOurText}</span>
       </h2>
     </div>
